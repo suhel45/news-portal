e23abf541141b5ff077110e12edba04ac670502c
suhel45 <suhel.ict45@gmail.com>
2022-09-03 01:27:31 +0600

    categories added

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('dlfkds')
+fetch('https://openapi.programming-hero.com/api/news/categories')
+.then(res => res.json())
+.then(data => display(data.data.news_category));
+
+
+const display = datas =>{
+    for(const data of datas){
+        const nav = document.getElementById('navbarNavAltMarkup');
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <a class="nav-link" href="#">${data.category_name}</a>
+        `
+        nav.appendChild(div);
+    }
+}
